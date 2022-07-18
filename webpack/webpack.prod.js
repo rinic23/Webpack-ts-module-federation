6cf63bbf126ba6f2d1396daa0cba80dc8f6317e4
Erichev Egor <ErichevEA@lad24.ru>
2022-07-18 23:21:13 +0300

    написать команду build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,6 +2,7 @@ const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "production",
+  devtool: "source-map",
   plugins: [
     new Dotenv({
       path: "./enviroment/production.env",
