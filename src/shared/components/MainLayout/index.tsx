49fcea504a72329c7895da7ed8ddfc8942888a8f
Erichev Egor <ErichevEA@lad24.ru>
2022-09-05 20:54:41 +0300

    исправить вёрстку

--- src/shared/components/MainLayout/index.tsx
+++ src/shared/components/MainLayout/index.tsx
@@ -10,10 +10,7 @@ type TProps = {
 export const MainLayout = ({ children }: TProps) => (
   <div className={s.main_layout}>
     <header className={s.header}>
-      <Link to={routes.breackingBad}>Во все тяжкие</Link>
-      <Link to={routes.breackingBad}>Во все тяжкие</Link>
-      <Link to={routes.breackingBad}>Во все тяжкие</Link>
-      <Link to={routes.breackingBad}>Во все тяжкие</Link>
+      <Link to={routes.main}>Главная</Link>
       <Link to={routes.breackingBad}>Во все тяжкие</Link>
     </header>
     <div className={s.content_wrapper}>{children}</div>
